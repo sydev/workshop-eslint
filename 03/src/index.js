41ef64f551550e8e511ece375d1335622fa50e26
Dominik Winter <330310-sydevde@users.noreply.gitlab.com>
2020-04-23 18:26:42 +0200

    add / Lesson 03 + 04

---
+++ 03/src/index.js
@@ -0,0 +1,5 @@
+var foo = NaN
+
+if (foo === NaN) {
+  console.log('foo === NaN');
+}
---
+++ 04/src/index.js
@@ -0,0 +1,11 @@
+var a = 1;
+
+const item = new Object();
+
+const atom = {
+  value: 1,
+
+  addValue: function (value) {
+    return atom.value + value;
+  },
+};
